024055dfc1691454d8c26d64b3f10b8854c5d161
setcookie <set@cooki.me>
2018-08-24 10:05:01 +0200

    add export functions

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -3,3 +3,4 @@
 require_once 'functions/common.php';
 require_once 'functions/datetime.php';
 require_once 'functions/object.php';
+require_once 'functions/export.php';
---
+++ lib/functions/export.php
@@ -0,0 +1,41 @@
+<?php
+
+if(!function_exists('dh_csv_export'))
+{
+    /**
+     * export/download csv data as string or array
+     *
+     * @param string|array|object $data expects the data to export
+     * @param string $name expects the file name
+     * @param string $delimiter expects the optional delimiter
+     * @param string $enclosure expects the optional enclosure
+     * @param string $escape_char expects the optional escape_char
+     */
+    function dh_csv_export($data, $name, $delimiter = ",", $enclosure = '"', $escape_char = "\\")
+    {
+        if(stripos($name, '.csv') === false)
+        {
+            $name = sprintf("%s.csv", trim($name));
+        }
+        header("Content-Description: File Transfer");
+        header("Content-Type: application/csv");
+        header("Content-Disposition: attachment; filename=$name");
+        header("Cache-Control: private", false);
+        header("Cache-Control: no-cache, no-store, must-revalidate");
+        header("Content-Transfer-Encoding: binary");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        if(is_array($data) || is_object($data))
+        {
+            $output = @fopen("php://output", "w");
+            foreach((array)$data as $d)
+            {
+                @fputcsv($output, $d, $delimiter, $enclosure, $escape_char);
+            }
+            @fclose($output);
+        }else{
+            echo (string)$data;
+        }
+        exit();
+    }
+}
