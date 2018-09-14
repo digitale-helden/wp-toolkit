1f8e7ff35927e6815e00e7cba3962af084f8bc49
setcookie <set@cooki.me>
2018-09-14 14:22:46 +0200

    add array helper

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -2,5 +2,6 @@
 
 require_once 'functions/common.php';
 require_once 'functions/datetime.php';
+require_once 'functions/array.php';
 require_once 'functions/object.php';
 require_once 'functions/export.php';
---
+++ lib/functions/array.php
@@ -0,0 +1,19 @@
+<?php
+
+if(!function_exists('dh_array_is_assoc'))
+{
+    /**
+     * check whether an array has only numeric keys
+     *
+     * @param mixed $array expects the array
+     * @return bool
+     */
+    function dh_array_is_assoc($array)
+    {
+        if(!is_array($array))
+        {
+            return false;
+        }
+        return count(array_filter(array_keys($array), 'is_string')) > 0;
+    }
+}
