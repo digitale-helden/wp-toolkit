b1561506c35cc700f28cdc8798458f23d06c16d1
setcookie <set@cooki.me>
2021-03-18 11:02:31 +0100

    adding common functions

--- lib/functions/common.php
+++ lib/functions/common.php
@@ -53,3 +53,99 @@ if(!function_exists('dh_is_value'))
         return true;
     }
 }
+
+if(!function_exists('dh_type'))
+{
+    /**
+     * get type of variable
+     *
+     * @param null $value
+     * @param false $convert
+     * @return string|null
+     */
+    function dh_type($value = null, $convert = false)
+    {
+        if(is_string($value) && (bool)$convert)
+        {
+            if(is_numeric($value))
+            {
+                if((float)$value != (int)$value){
+                    $value = (float)$value;
+                }else{
+                    $value = (int)$value;
+               }
+            }else{
+                if($value === 'true' || $value === 'false')
+                {
+                    $value = (bool)$value;
+                }
+            }
+        }
+
+        if(is_object($value)){
+            return 'object';
+        }
+        if(is_array($value)){
+            return 'array';
+        }
+        if(is_resource($value)){
+            return 'resource';
+        }
+        if(is_callable($value)){
+            return 'callable';
+        }
+        if(is_file($value)){
+            return 'file';
+        }
+        if(is_int($value)){
+            return 'integer';
+        }
+        if(is_float($value)){
+            return 'float';
+        }
+        if(is_bool($value)){
+            return 'boolean';
+        }
+        if(is_null($value)){
+            return 'null';
+        }
+        if(is_string($value)){
+            return 'string';
+        }
+        return null;
+    }
+}
+
+
+if(!function_exists('dh_normalize'))
+{
+    /**
+     * normalize = typify values for use in json objects eg
+     * @param $value
+     * @return bool|float|int|string|null
+     */
+    function dh_normalize($value)
+    {
+        if(is_numeric($value) && (int)$value <= PHP_INT_MAX)
+        {
+            if((int)$value != $value){
+                return (float)$value;
+            }else if(filter_var($value, FILTER_VALIDATE_INT) !== false){
+                return (int)$value;
+            }else{
+                return strval($value);
+            }
+        }else{
+            if($value === 'true' || $value === 'TRUE')
+            {
+                return true;
+            }else if($value === 'false' || $value === 'FALSE'){
+                return false;
+            }else if($value === 'null' || $value === 'NULL'){
+                return null;
+            }else{
+                return strval($value);
+            }
+        }
+    }
+}
